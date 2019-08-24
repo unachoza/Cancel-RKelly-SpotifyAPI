fb0ca9e81db39db00a4d711fb478982981489eb8
Arianna Choza <unachoza@gmail.com>
2019-08-24 12:08:24 -0400

    added button

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="http://localhost:8888">
+        <button>Login With Spotify</button>
+      </a>
+    </div>
+  );
+}
+
+export default App;
