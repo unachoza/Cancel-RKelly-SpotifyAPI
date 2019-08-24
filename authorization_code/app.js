318897adf4b6c6bd2100dd40eb175bb10ad65b35
Arianna Choza <unachoza@gmail.com>
2019-08-24 11:33:51 -0400

    cloned

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,7 +13,7 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
+var client_id = 'CLIENT'; // Your client id
 var client_secret = 'CLIENT_SECRET'; // Your secret
 var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
 
