0bd080c7b2cd823151541544aa8523ca19a4159c
Arianna Choza <unachoza@gmail.com>
2019-08-24 12:06:57 -0400

    works

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '724c6815dc24749af9bfd50cc601061'; // Your client id
+var client_secret = '668836e2f7994d939fe6b12727109a84'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
